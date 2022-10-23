edeb5196e6f4eed80b9441b9a10da14680c39f5c
conwys <drkheartt@gmail.com>
2022-10-23 15:29:50 +0100

    routing and modules done

--- models/users.js
+++ models/users.js
@@ -3,15 +3,74 @@ const path = require("node:path");
 const { v4: uuidv4 } = require("uuid");
 const filePath = path.resolve(process.cwd(), "data", "users.json");
 
-async function getUsers() {}
+async function getUsers() {
+  const usersJSON = await fs.readFile(filePath);
+  const users = JSON.parse(usersJSON);
+  return users;
+}
 
-async function getUserByID(id) {}
+async function getUserByID(id) {
+  const usersJSON = await fs.readFile(filePath);
+  const users = JSON.parse(usersJSON);
+  let user = null;
+  for (let i=0; i<users.length; i++) {
+    if (users[i].id === id) {
+      user = users[i];
+    }
+  }
+  return user;
+}
 
-async function createUser(newUser) {}
+async function createUser(newUser) {
+  const usersJSON = await fs.readFile(filePath);
+  const users = JSON.parse(usersJSON);
+  const userToAdd = {
+    id: uuidv4(),
+    first_name: newUser.first_name,
+    last_name: newUser.last_name,
+    email: newUser.email,
+    catchphrase: newUser.catchphrase,
+  }
+  users.push(userToAdd);
+  await fs.writeFile(filePath, JSON.stringify(users));
+  return userToAdd;
+}
 
-async function updateUserByID(id, updatedUser) {}
+async function updateUserByID(id, updatedUser) {
+  const usersJSON = await fs.readFile(filePath);
+  const users = JSON.parse(usersJSON);
+  let user = null;
+  for (let i=0; i<users.length; i++) {
+    if (users[i].id === id) {
+      user = users[i];
+      user.first_name = updatedUser.first_name;
+      user.last_name = updatedUser.last_name;
+      user.email = updatedUser.email;
+      user.catchphrase = updatedUser.catchphrase;
+    }
+  }
+  await fs.writeFile(filePath, JSON.stringify(users));
 
-async function deleteUserByID(id) {}
+  return updatedUser;
+}
+
+async function deleteUserByID(id) {
+  const usersJSON = await fs.readFile(filePath);
+  const users = JSON.parse(usersJSON);
+  let userIndex = null;
+  for (let i=0; i<users.length; i++) {
+    if (users[i].id === id) {
+      userIndex = i;
+      break;
+    }
+  }
+  if (userIndex !== null){
+    const deletedUser = users.splice(userIndex, 1);
+    await fs.writeFile(filePath, JSON.stringify(users));
+    return deletedUser;
+  }
+  return null;
+}
 
 module.exports = {
   getUsers,
